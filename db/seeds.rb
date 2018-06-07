238080e1fa25eb9978456b56fdccc91d3e30452e
beranemo <beranemo@gmail.com>
2018-06-07 23:49:43 +0800

    generate the model and set data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+r1 = Role.create(name: 'user')  
+r2 = Role.create(name: 'admin')  
+u1 = User.create(email: 'user@xxx.com' , password: '12345678', role_id: r1.id)   
+u2 = User.create(email: 'admin@xx.com' , password: 'q1w2e3r4', role_id: r2.id)
